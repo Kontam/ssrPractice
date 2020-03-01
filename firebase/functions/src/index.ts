24c4eea7f1db9e0f9e949863c0dcde66bc46c974
Kanta <tehayan0111@gmail.com>
2020-03-01 18:57:54 +0900

    不要なデバッグコードを削除

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -6,10 +6,70 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
-export const mockLongo = functions.https.onRequest(async (request, response) => {
-    const collection = admin.firestore().collection("longs");
-    console.log(collection);
-    const data = await collection.doc("longo").set({a: "anan"});
-    console.log(data);
-    response.end();
+type Longo = {
+    id: string,
+    text: string,
+    meaning: string,
+    comment: string,
+}
+
+/**
+ * 渡されたidのデータが存在するかをチェックする
+ * @param ref チェック対象のcollectionのref
+ * @param id チェック対象データのid
+ * @return {boolean} 存在すればfalse
+ */
+async function checkIsEmptyById(ref :any, id: string) {
+    return await (await ref.where("id", "==", id).get()).empty
+}
+
+export const longoAPI = functions.https.onRequest(async (request, response) => {
+    const ref = admin.firestore().collection("Longos");
+    switch (request.method) {
+        case "GET" :
+            const snapshot = await ref.get();
+            if (snapshot.empty) {
+                response.send({});
+            }
+            const longos :Longo[] = [];
+            snapshot.forEach(doc => {
+                longos.push({
+                    ...(doc.data() as Longo),
+                    id: doc.id,
+                });
+            })
+            response.send(longos);
+            break;
+
+        case "PATCH":
+            const params: Longo = request.body;
+            if (await checkIsEmptyById(ref, params.id)) {
+                response.send(`${params.id} is not exist`);
+                break;
+            }
+            await ref.doc(params.id).set(params);
+            response.send(params);
+            break;
+        
+        case "POST":
+            const newPost: Partial<Longo> = request.body;
+            //TODO: エラーチェックが公式に書いてないのでなにか考えたい
+             await ref.add(newPost);
+            response.send(newPost);
+            break;
+        
+        case "DELETE":
+            const deleteId: {id: string} = request.body;
+            if (! await checkIsEmptyById(ref, deleteId.id)) {
+                response.send(`${deleteId.id} is not exist`);
+                break;
+            }
+            //TODO: エラーチェックが公式に書いてないのでなにか考えたい
+            await ref.doc(deleteId.id).delete();
+            response.send(deleteId);
+            break;
+
+        default :
+            console.log(request.method);
+    }
 })
