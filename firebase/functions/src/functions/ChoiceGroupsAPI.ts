8402996cc1614a27233483c07ecea41503036caf
Kanta <tehayan0111@gmail.com>
2020-04-04 16:27:54 +0900

    functions update追加
    - update処理を追加

--- firebase/functions/src/functions/ChoiceGroupsAPI.ts
+++ firebase/functions/src/functions/ChoiceGroupsAPI.ts
@@ -1,4 +1,5 @@
 import admin from '../modules/firebaseAdmin';
+import * as firebase from 'firebase-admin';
 import { Request, Response } from 'firebase-functions';
 import { getOptionsByGroupId } from './ChoiceOptionsAPI';
 
@@ -16,7 +17,7 @@ export type ChoiceGroupDB = {
 export type ChoiceOptionDB = {
   groupId: string,
   choiceEnabled: boolean,
-  createdAt: string,
+  createdAt: firebase.firestore.FieldValue,
   choiceName: string,
 }
 
@@ -38,6 +39,7 @@ export type ChoiceGroup = {
   choiceOptions: ChoiceOption[], 
 }
 
+
 const CHOICE_GROUPS = "ChoiceGroups" as const;
 const CHOICE_OPTIONS = "ChoiceOptions" as const;
 
@@ -47,6 +49,7 @@ const CHOICE_OPTIONS = "ChoiceOptions" as const;
  * */
 export default async function choiseGroupsAPI(request: Request, response: Response): Promise<void>{
   const firestore = admin.firestore();
+  const timeStamp = admin.firestore.FieldValue.serverTimestamp();
   const groupRef = firestore.collection(CHOICE_GROUPS);
   const optionRef = firestore.collection(CHOICE_OPTIONS);
   switch (request.method) {
@@ -71,7 +74,6 @@ export default async function choiseGroupsAPI(request: Request, response: Respon
 
     case "POST" :
       const newPost: Partial<ChoiceGroup> = request.body;
-      const timeStamp = admin.firestore.FieldValue.serverTimestamp();
       // choiceOptionsとchoiceGroupは別のコレクション
       // groupとoptionの紐付きはoption側で親のIDを保持して実現する
       const newGroupRef = await groupRef.add(
@@ -113,11 +115,56 @@ export default async function choiseGroupsAPI(request: Request, response: Respon
         groupName: groupData && groupData.groupName || "", 
         choiceOptions: optionsData || [],
       }
-      console.log("optionsData", optionsData);
       response.send(res);
+      break;
+
+    case "PATCH" :
+      const patchPost: ChoiceGroup = request.body;
+      const groupDoc = groupRef.doc(patchPost.groupId);
+      groupDoc.update({groupName: patchPost.groupName});
+
+      const patchOptionBatch = firestore.batch();
+      // Optionsの数は減少する可能性があるのでDelete then insert
+      const optionQuerySnapshot = await optionRef.where("groupId", "==", patchPost.groupId).get();
+      optionQuerySnapshot.forEach((docSnap) => {
+        patchOptionBatch.delete(docSnap.ref);
+      });
+      patchOptionBatch.commit();
+      
+      const patchInsertedData = await Promise.all(patchPost.choiceOptions.map((option) => {
+        const insertData: ChoiceOptionDB = {
+          groupId: patchPost.groupId,
+          choiceName: option.choiceName,
+          choiceEnabled: option.choiceEnabled,
+          createdAt: timeStamp, 
+        }
+        return optionRef.add(insertData);
+      }));
+
+      const patchResponseOptions: ChoiceOption[] = await Promise.all(patchInsertedData.map( async (docRef) => {
+        const patchSnapshot = await docRef.get();
+        const patchData = patchSnapshot.data();
+        return {
+          choiceId: docRef.id,
+          choiceName: patchData!.choiceName,
+          choiceEnabled: patchData!.choiceEnabled,
+        };
+      }));
+
+      const patchTargetSnap = await groupDoc.get();
+      const patchedData = patchTargetSnap.data();
+      const patchResponseGroup: ChoiceGroup = {
+        groupId: groupDoc.id,
+        groupName: patchedData && patchedData.groupName || "",
+        choiceOptions: patchResponseOptions,
+      }
+      response.send(patchResponseGroup);
       break;
 
     default:
       response.send("default");
   }
+
+   
+
 }
