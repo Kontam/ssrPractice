486164d2f2888e42b51dc5e38bb405440bfe300c
Kanta <tehayan0111@gmail.com>
2020-04-08 01:01:33 +0900

    front ChoiceForm チェックボックスの表示
    - チェックon offは不可だが、stateに応じたチェックボックスを追加

--- src/shared/components/atoms/FieldArrayText/index.tsx
+++ src/shared/components/atoms/FieldArrayText/index.tsx
@@ -2,11 +2,15 @@ import React, { useState, ChangeEvent } from 'react';
 import styled from 'styled-components';
 import { WrappedFieldArrayProps, EventWithDataHandler } from 'redux-form';
 import FormTextField from '../FormTextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { ChoiceFormData } from '../../molecules/ChoiceForm';
+import { ChoiceOption } from '../../../../../firebase/functions/src/functions/ChoiceGroupsAPI';
 
 const OptionList = styled.ul``;
 const OptionItem = styled.li``;
 
-const FieldArrayText = ({ fields, meta: { error }}: WrappedFieldArrayProps) => { 
+const FieldArrayText = ({ fields, meta: { error }}: WrappedFieldArrayProps<ChoiceOption>) => { 
     const [added, setAdded] = useState<boolean[]>([]);
 
     return (
@@ -14,12 +18,17 @@ const FieldArrayText = ({ fields, meta: { error }}: WrappedFieldArrayProps) => {
             {fields.map((option, index) => {
                 const onChange: EventWithDataHandler<ChangeEvent<any>> = (e, newValue, previewsValue) => {
                     if (!previewsValue && !added[index]) {
-                        fields.push({});
+                        fields.push({
+                            choiceName: "",
+                            choiceEnabled: true,
+                            choiceId: "",
+                        });
                         const newAdded = [...added];
                         newAdded[index] = true;
                         setAdded(newAdded)
                     }
                 } 
+                const data = fields.get(index);
                 return ( 
                     <OptionItem key={index}>
                         <FormTextField 
@@ -27,6 +36,13 @@ const FieldArrayText = ({ fields, meta: { error }}: WrappedFieldArrayProps) => {
                             label={`名前${index + 1}`}
                             onChange={onChange}
                         />
+                        <FormControlLabel
+                            value="有効"
+                            control={<Checkbox color="primary" checked={data.choiceEnabled}/>}
+                            label="有効"
+                            labelPlacement="start"
+                        />
+
                     </OptionItem>
             )})}
         </OptionList>
