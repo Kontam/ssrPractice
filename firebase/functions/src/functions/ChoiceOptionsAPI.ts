63f578ab048e5717cdebdfb8e0cd1cd5cf683484
Kanta <tehayan0111@gmail.com>
2020-04-11 23:39:23 +0900

    functions オプションチョイスAPI作成
    - ChoiceOptionAPI作成 Enabledが考慮されていない

--- firebase/functions/src/functions/ChoiceOptionsAPI.ts
+++ firebase/functions/src/functions/ChoiceOptionsAPI.ts
@@ -1,9 +1,12 @@
-import * as admin from 'firebase-admin';
-import { ChoiceOption } from './ChoiceGroupsAPI';
+import * as firebase from 'firebase-admin';
+import { Request, Response } from 'express';
+import { ChoiceOption } from '../types';
+import admin from '../modules/firebaseAdmin';
+import { CHOICE_GROUPS, CHOICE_OPTIONS } from './ChoiceGroupsAPI';
+import { chooseItemsRandomly } from '../modules/util';
 
-
-export const getOptionsByGroupId = async (groupId: string, groupRef:admin.firestore.CollectionReference): Promise<ChoiceOption[]> => {
-  const snapshot = await groupRef.where('groupId', '==', groupId).get(); 
+export const getOptionsByGroupId = async (groupId: string, optionRef:firebase.firestore.CollectionReference): Promise<ChoiceOption[]> => {
+  const snapshot = await optionRef.where('groupId', '==', groupId).get(); 
   const choiceOptions :ChoiceOption[] = snapshot.docs.map((doc) => {
     const data = doc.data();
     return ({
@@ -14,4 +17,55 @@ export const getOptionsByGroupId = async (groupId: string, groupRef:admin.firest
   });
 
   return choiceOptions;
+}
+
+/**
+ * グループ名から対応するオプションの配列を検索する
+ * 本来ないはずだが、複数ヒットした場合は0番目を返す
+ * @param groupName グループ名
+ * @param groupRef グループコレクションのリファレンス
+ * @param optionRef オプションコレクションのリファレンス
+ */
+export const getOptionsByGroupName = async (
+  groupName: string,
+  groupRef:firebase.firestore.CollectionReference,
+  optionRef:firebase.firestore.CollectionReference
+): Promise<ChoiceOption[]> => {
+  const snapshot = await groupRef.where('groupName', '==', groupName).get(); 
+  const groupIds = await snapshot.docs.map((doc) => doc.id);
+  const optionSnap = await optionRef.where('groupId', '==', groupIds[0]).get();
+  const choiceOptions :ChoiceOption[] = optionSnap.docs.map((doc) => {
+    const data = doc.data();
+    return ({
+      choiceId: doc.id,
+      choiceName: data.choiceName,
+      choiceEnabled: data.choiceEnabled,
+    });
+  });
+
+  return choiceOptions;
+}
+
+export default async function ChoiceOptionAPI(req: Request, res: Response) {
+  const firestore = admin.firestore();
+  const groupRef = firestore.collection(CHOICE_GROUPS);
+  const optionRef = firestore.collection(CHOICE_OPTIONS)
+  console.log(req.query);
+
+  switch(req.method) {
+    case "GET":
+      if (!req.query.groupName) res.send("invalid request");
+      let groupName: string = req.query.groupName;
+      let amount = 0
+      if (req.query.amount) {
+        amount = parseInt(req.query.amount);
+      }
+
+      const Options = await getOptionsByGroupName(groupName, groupRef, optionRef); 
+      console.log("Options", Options);
+      const choosenOptions = chooseItemsRandomly(Options, amount);
+      
+      res.send(choosenOptions);
+  }
+
 }
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 import longoAPIfunc from './functions/LongoAPI';
 import authorityAPIfunc from './functions/AuthorityAPI';
 import choiseGroupAPIfunc from './functions/ChoiceGroupsAPI';
+import choiseOptionAPIfunc from './functions/ChoiceOptionsAPI';
 
 export type Longo = {
     id: string,
@@ -13,3 +14,4 @@ export type Longo = {
 export const longoAPI = functions.https.onRequest(longoAPIfunc);
 export const authorityAPI = functions.https.onRequest(authorityAPIfunc);
 export const choiceGroupAPI = functions.https.onRequest(choiseGroupAPIfunc);
+export const choiceOptionAPI = functions.https.onRequest(choiseOptionAPIfunc);
