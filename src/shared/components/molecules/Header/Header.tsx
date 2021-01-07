210a16a55588b4a6525fcc1bd820f5fcc3634413
Kanta <tehayan0111@gmail.com>
2021-01-08 00:21:13 +0900

    feature: ヘッダーのスタイルを変更　ボタンを右寄せに

--- src/shared/components/molecules/Header/Header.tsx
+++ src/shared/components/molecules/Header/Header.tsx
@@ -1,46 +1,53 @@
-
-import React from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import HeaderMenuItem from '../../atoms/HeaderMenuItem';
-import { Toolbar } from '@material-ui/core';
-import LoadingLine from '../../atoms/LoadingLine';
-import { HeaderLoading } from '../../../redux/modules/headerLoading';
-import AppButtonContainer, { AppFAB } from '../AppButtonContainer';
+import React from "react";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import HeaderMenuItem from "../../atoms/HeaderMenuItem";
+import { Toolbar } from "@material-ui/core";
+import LoadingLine from "../../atoms/LoadingLine";
+import { HeaderLoading } from "../../../redux/modules/headerLoading";
+import AppButtonContainer, { AppFAB } from "../AppButtonContainer";
 
 export type NavMenu = {
-    text: string,
-    href: string,
-    description: string,
-}
+  text: string;
+  href: string;
+  description: string;
+};
 
-const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        color: "white",
-    }
-}))
+const useStyles = makeStyles(() => ({
+  root: {
+    color: "white",
+  },
+  grow: {
+    display: 'flex',
+    flexGrow: 1,
+  },
+}));
 
 export type HeaderProps = {
-    navMenus: NavMenu[]
-    headerLoading: HeaderLoading
-    appButtons?: AppFAB[],
-}
+  navMenus: NavMenu[];
+  headerLoading: HeaderLoading;
+  appButtons?: AppFAB[];
+};
 
-const Header: React.FC<HeaderProps> = ({ navMenus, headerLoading, appButtons }) => {
-    
-    return (
-        <AppBar position="sticky">
-            <Toolbar>
-            {/* <Nav> */}
-                {/* <MenuList> */}
-                    {navMenus.map((menu) => <HeaderMenuItem key={menu.text} menu={menu}/>)}
-                {/* </MenuList> */}
-            {/* </Nav> */}
-            { appButtons && <AppButtonContainer appButtons={appButtons}/> }
-            </Toolbar>
-            <LoadingLine isLoading={headerLoading} />
-        </AppBar>
-    );
-}
+const Header: React.FC<HeaderProps> = ({
+  navMenus,
+  headerLoading,
+  appButtons,
+}) => {
+  const classes = useStyles();
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <div className={classes.grow}>
+          {navMenus.map((menu) => (
+            <HeaderMenuItem key={menu.text} menu={menu} />
+          ))}
+        </div>
+        {appButtons && <AppButtonContainer appButtons={appButtons} />}
+      </Toolbar>
+      <LoadingLine isLoading={headerLoading} />
+    </AppBar>
+  );
+};
 
 export default Header;
--- src/shared/components/molecules/Header/index.tsx
+++ src/shared/components/molecules/Header/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Header, { NavMenu } from './Header';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/store';
-import { closeAddDialog, AddDialogState, openAddDialog } from '../../../redux/modules/addDialogState';
 import { HeaderLoading } from '../../../redux/modules/headerLoading';
 import { AppFAB } from '../AppButtonContainer';
 
@@ -11,8 +10,6 @@ type Props = {
 }
 
 const HeaderContainer: React.FC<Props> = ({ appButtons }) => {
-    const dispatch = useDispatch();
-    const addDialogState = useSelector<RootState, AddDialogState>(state => state.dialog.addDialogState);
     const headerLoading = useSelector<RootState, HeaderLoading>(state => state.headerLoading);
     const navMenus: NavMenu[] = [
         {
@@ -32,13 +29,10 @@ const HeaderContainer: React.FC<Props> = ({ appButtons }) => {
         }
     ];
 
-    const handleAddIconClick: React.MouseEventHandler = () => { dispatch(addDialogState.isOpen ? closeAddDialog() : openAddDialog())};
-    
     return (
         <Header
             appButtons={appButtons}
             navMenus={navMenus} 
-            handleAddIconClick={handleAddIconClick}
             headerLoading={headerLoading}
         />
     );
