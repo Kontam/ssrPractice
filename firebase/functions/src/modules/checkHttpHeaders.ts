648438b6a43c18367cbdfa4ac097c93a95bf1e31
Kontam <tehayan0111@gmail.com>
2021-02-07 16:42:42 +0900

    fix: checkHttpHeadersのapiKey不正でもtrueになっていた不具合の修正

--- firebase/functions/src/modules/checkHttpHeaders.ts
+++ firebase/functions/src/modules/checkHttpHeaders.ts
@@ -1,32 +1,29 @@
-import * as functions from 'firebase-functions';
-import * as jwt from 'jsonwebtoken';
-import { HttpsError } from 'firebase-functions/lib/providers/https';
+import * as functions from "firebase-functions";
+import * as jwt from "jsonwebtoken";
 
-export function checkHttpHeaders(request: functions.Request, response: functions.Response) {
-  const headers = request.headers;
-  console.log(headers);
-  const apiToken = request.header("x-api-key"); 
+export function checkHttpHeaders(
+  request: functions.Request,
+  response: functions.Response
+) {
+  const apiToken = request.header("x-api-key");
   if (!apiToken) {
-    console.error("missing api token");
-    throw new HttpsError("invalid-argument", "Invalid Argument")
     response.send({
       error: true,
-      reason: "missing api key on request",
+      reason: "missing api key on request"
     });
     return false;
   }
   try {
     const apisecret = functions.config().general.apisecret;
     const decoded = jwt.verify(apiToken, apisecret);
-    if (!decoded === functions.config().general.apikey) throw new Error("invalid apiKey"); 
+    if (decoded !== functions.config().general.apiKey)
+      throw new Error(apisecret);
     return true;
-  } catch {
-    throw new HttpsError("invalid-argument", "Invalid Argument")
+  } catch(e) {
     response.send({
       error: true,
-      reason: "invalid api token",
+      reason: `invalid api token`
     });
-    console.error("invalid api token");
     return false;
   }
 }
