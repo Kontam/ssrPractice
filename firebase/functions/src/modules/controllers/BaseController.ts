402e9557224ee9ed9f1c36ce9f9455db8e3cfd2a
Kontam <tehayan0111@gmail.com>
2021-02-14 15:30:01 +0900

    feat: add check body parametors

--- firebase/functions/src/modules/controllers/BaseController.ts
+++ firebase/functions/src/modules/controllers/BaseController.ts
@@ -11,6 +11,12 @@ export class BaseController {
   constructor() {
     this.paramTypes = new Map();
     this.bodyTypes = new Map();
+    this.paramTypes.set("get", [])
+    this.paramTypes.set("post", [])
+    this.paramTypes.set("patch", [])
+    this.paramTypes.set("delete", [])
+    this.bodyTypes.set("post", [])
+    this.bodyTypes.set("patch", [])
   }
 
   get(req: Request, res: Response): any {
@@ -19,10 +25,12 @@ export class BaseController {
 
   post(req: Request, res: Response): any {
     this._setup(req, res, 'post');
+    this._checkBody(req, 'post');
   }
 
   patch(req: Request, res: Response): any {
     this._setup(req, res, 'patch');
+    this._checkBody(req, 'patch');
   }
 
   delete(req: Request, res: Response): any {
@@ -34,12 +42,21 @@ export class BaseController {
       return;
 
     const paramType = this.paramTypes.get(method);
-    if (!paramType) {
-      console.error(`${method} paramtypes are undefined`);
-      return; 
-    }
+    if (!paramType) 
+      throw new SuebotAPIExeption(`${method} query parameters are undefined`);
+
     const invalidQuery = filterValidParametors(req.query, paramType);
     if (invalidQuery.length === 0) return;
     throw new SuebotAPIExeption(`invalid: ${invalidQuery[0][0]}`)
+  }
+
+  _checkBody(req: Request, method: "post" | "patch") {
+    const bodyType = this.bodyTypes.get(method); 
+    if (!bodyType) 
+      throw new SuebotAPIExeption(`${method} body parameters are undefined`);
+    
+    const invalidBody = filterValidParametors(req.body, bodyType);
+    if (invalidBody.length === 0) return;
+    throw new SuebotAPIExeption(`invalid body: ${invalidBody[0][0]}`)
   }
 }
--- firebase/functions/src/modules/controllers/__tests__/BaseController.test.ts
+++ firebase/functions/src/modules/controllers/__tests__/BaseController.test.ts
@@ -17,87 +17,128 @@ const mockConfig = {
 (functions as jest.Mock).config.mockReturnValue(mockConfig);
 
 describe("BaseController", () => {
-  let baseController = new BaseController();
   let mockReq: Request;
   let mockRes: Response;
   beforeEach(() => {
     mockRes = {} as any;
   });
 
-  describe.each([
-    ["get", baseController.get.bind(baseController)],
-    ["post", baseController.post.bind(baseController)],
-    ["patch", baseController.post.bind(baseController)],
-    ["delete", baseController.post.bind(baseController)],
-  ])("%s: APIキーが存在していない時", (_, method) => {
-    beforeEach(() => {
-      mockReq = {
-        header: () => undefined,
-      } as any;
-    });
+  describe("APIKey 異常系", () => {
+    let baseController = new BaseController();
+    describe.each([
+      ["get", baseController.get.bind(baseController)],
+      ["post", baseController.post.bind(baseController)],
+      ["patch", baseController.patch.bind(baseController)],
+      ["delete", baseController.delete.bind(baseController)],
+    ])("%s: APIキーが存在していない時", (_, method) => {
+      beforeEach(() => {
+        mockReq = {
+          header: () => undefined,
+        } as any;
+      });
 
-    test("APIキー不足のExceptionがthrowされる", () => {
-      expect.assertions(1);
-      try {
-        method(mockReq, mockRes);
-      } catch (e) {
-        expect(e.reason).toStrictEqual("missing api key");
-      }
-    });
-  });
-
-  describe.each([
-    ["get", baseController.get.bind(baseController)],
-    ["post", baseController.post.bind(baseController)],
-    ["patch", baseController.post.bind(baseController)],
-    ["delete", baseController.post.bind(baseController)],
-  ])("%s: 想定されたリクエストが渡された時", (_, method) => {
-    beforeEach(() => {
-      baseController.paramTypes.set("get", [['param1', 'string']])
-      baseController.paramTypes.set("post", [['param1', 'string']])
-      baseController.paramTypes.set("patch", [['param1', 'string']])
-      baseController.paramTypes.set("delete", [['param1', 'string']])
-      mockReq = {
-        header: () => 'apikey',
-        query: { param1: 'test' },
-      } as any
-
-      //@ts-ignore
-      (jwt.verify as jest.Mock).mockReturnValue(VALID_TOKEN);
-    });
-
-    test("例外が発生しない", () => {
-      method(mockReq, mockRes);
+      test("APIキー不足のExceptionがthrowされる", () => {
+        expect.assertions(1);
+        try {
+          method(mockReq, mockRes);
+        } catch (e) {
+          expect(e.reason).toStrictEqual("missing api key");
+        }
+      });
     });
   });
 
-  describe.each([
-    ["get", baseController.get.bind(baseController)],
-    ["post", baseController.post.bind(baseController)],
-    ["patch", baseController.post.bind(baseController)],
-    ["delete", baseController.post.bind(baseController)],
-  ])("%s: 要求getパラメーターに不足がある時", (_, method) => {
-    beforeEach(() => {
-      baseController.paramTypes.set("get", [['param1', 'string']])
-      baseController.paramTypes.set("post", [['param1', 'string']])
-      baseController.paramTypes.set("patch", [['param1', 'string']])
-      baseController.paramTypes.set("delete", [['param1', 'string']])
-      mockReq = {
-        header: () => 'apikey',
-        query: {},
-      } as any
+  describe("Queryチェック 正常系", () => {
+    let baseController = new BaseController();
+    describe.each([
+      ["get", baseController.get.bind(baseController)],
+      ["post", baseController.post.bind(baseController)],
+      ["patch", baseController.patch.bind(baseController)],
+      ["delete", baseController.delete.bind(baseController)],
+    ])("%s: 想定されたリクエストが渡された時", (_, method) => {
+      beforeEach(() => {
+        baseController.paramTypes.set("get", [['param1', 'string']])
+        baseController.paramTypes.set("post", [['param1', 'string']])
+        baseController.paramTypes.set("patch", [['param1', 'string']])
+        baseController.paramTypes.set("delete", [['param1', 'string']])
+        baseController.bodyTypes.set("post", [['body1', 'string']])
+        baseController.bodyTypes.set("patch", [['body1', 'string']])
+        mockReq = {
+          header: () => 'apikey',
+          query: { param1: 'test' },
+          body: { body1: 'body' }
+        } as any
 
-      //@ts-ignore
-      (jwt.verify as jest.Mock).mockReturnValue(VALID_TOKEN);
-    });
+        //@ts-ignore
+        (jwt.verify as jest.Mock).mockReturnValue(VALID_TOKEN);
+      });
 
-    test("パラメータ不正のExceptionがthrowされる", () => {
-      expect.assertions(1);
-      try {
+      test("例外が発生しない", () => {
         method(mockReq, mockRes);
-      } catch (e) {
-        expect(e.reason).toStrictEqual("invalid: param1");
-      }
+      });
+    });
+  });
+
+  describe("Queryチェック 異常系", () => {
+    let baseController = new BaseController();
+    describe.each([
+      ["get", baseController.get.bind(baseController)],
+      ["post", baseController.post.bind(baseController)],
+      ["patch", baseController.patch.bind(baseController)],
+      ["delete", baseController.delete.bind(baseController)],
+    ])("%s: 要求getパラメーターに不足がある時", (_, method) => {
+      beforeEach(() => {
+        baseController.paramTypes.set("get", [['param1', 'string']])
+        baseController.paramTypes.set("post", [['param1', 'string']])
+        baseController.paramTypes.set("patch", [['param1', 'string']])
+        baseController.paramTypes.set("delete", [['param1', 'string']])
+        mockReq = {
+          header: () => 'apikey',
+          query: {},
+        } as any
+
+        //@ts-ignore
+        (jwt.verify as jest.Mock).mockReturnValue(VALID_TOKEN);
+      });
+
+      test("パラメータ不正のExceptionがthrowされる", () => {
+        expect.assertions(1);
+        try {
+          method(mockReq, mockRes);
+        } catch (e) {
+          expect(e.reason).toStrictEqual("invalid: param1");
+        }
+      });
+    });
+  });
+
+  describe("Bodyチェック 異常系", () => {
+    let baseController = new BaseController();
+    describe.each([
+      ["post", baseController.post.bind(baseController)],
+      ["patch", baseController.patch.bind(baseController)],
+    ])("%s: 要求bodyパラメーターに不足がある時", (_, method) => {
+      beforeEach(() => {
+        baseController.bodyTypes.set("post", [['body1', 'string']])
+        baseController.bodyTypes.set("patch", [['body1', 'string']])
+        mockReq = {
+          header: () => 'apikey',
+          query: {},
+          body: {},
+        } as any
+
+        //@ts-ignore
+        (jwt.verify as jest.Mock).mockReturnValue(VALID_TOKEN);
+      });
+
+      test("パラメータ不正のExceptionがthrowされる", () => {
+        expect.assertions(1);
+        try {
+          method(mockReq, mockRes);
+        } catch (e) {
+          expect(e.reason).toStrictEqual("invalid body: body1");
+        }
+      });
     });
   });
 });
